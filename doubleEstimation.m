ec1fd49bedfa065f8d5c6d56f61d81862f355ffb
Paul Boutet <paul@nboutet.com>
2025-01-08 11:50:38 +0100

    add comments on doubleEstimation.m

--- doubleEstimation.m
+++ doubleEstimation.m
@@ -1,4 +1,10 @@
 function [result, consistency] = doubleEstimation(image)
+    % Double estimation of the beacon (triangle and color)
+    % input : the image
+    % output : the estimation of the beacon and a boolean that indicates if the estimation is consistent
+    % If the estimations of the triangle and the color are the same, the estimation is consistent
+    % Otherwise, the estimation is not consistent and the result is the two estimations
+    % separated by "or". The first should be the triangle estimation and the second the color estimation
 
     mask = beaconMask(image);
 
